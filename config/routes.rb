e7d89cbd17a21813ec9ef9907f6476a6459d4647
Jim Knight <jim.knight@lavatech.com>
2015-02-08 15:06:55 -0500

    got the home page looking all pretty with fake content

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users
-    root to: "home#index"
+  root :to => "pages#home", :id => 'home'
 
-    mount Koudoku::Engine, at: 'koudoku'
-scope module: 'koudoku' do
-  get 'pricing' => 'subscriptions#index', as: 'pricing'
-end
+  mount Koudoku::Engine, at: 'koudoku'
+  scope module: 'koudoku' do
+    get 'pricing' => 'subscriptions#index', as: 'pricing'
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
